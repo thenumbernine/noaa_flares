75ab90776bcfd7ca0d7b6f8d9a69122a4b00b8d2
thenumbernine <christopher.e.moore@gmail.com>
2022-09-16 01:33:21 +0700

    updating to new ext.file

--- nc_to_txt.lua
+++ nc_to_txt.lua
@@ -1,9 +1,16 @@
 #!/usr/bin/env luajit
 local os = require 'ext.os'
-local io = require 'ext.io'
-for f in os.listdir'nc' do
-	local base, ext = io.getfileext(f)
+local file = require 'ext.file'
+for f in file'nc':dir() do
+	local base, ext = file(f):getext()
 	assert(ext == 'nc')
-	print(base, ext)
-	os.execute('../netcdf/test.lua nc/'..f..' > nc_txt/'..base..'.txt')
+	--io.write(base, ' ', ext)
+	local dst = 'nc_txt/'..base..'.txt'
+	io.write(dst)
+	if file(dst):exists() then	-- TODO check timestamp?
+		print(' ... exists')
+	else
+		os.execute('../netcdf/test.lua nc/'..f..' > '..dst)
+		print(' ... done')
+	end
 end
