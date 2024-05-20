c53e093adfac12111f12179cab348726a3b7e81e
thenumbernine <christopher.e.moore@gmail.com>
2024-05-20 10:40:06 +0800

    more

---
+++ smoothplot.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+../smooth_graph/smooth_graph.lua totalflares-per-month.txt 50 totalflares-per-month-smoothed.svg
--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 ./nc_to_txt.lua
 ./count-flares.lua
 ./plot.gnuplot
-../smooth_graph/smooth_graph.lua totalflares-per-month.txt 50 totalflares-per-month-smoothed.svg
+./smoothplot.sh
 #git commit -am more && git push
