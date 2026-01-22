fe776045f6b3524ac8a4be2f9bb689cb4800bb61
Christopher Moore <christopher.e.moore@gmail.com>
2026-01-22 12:06:44 +0700

    more

--- smoothplot.sh
+++ smoothplot.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-../smooth_graph/smooth_graph.lua totalflares-per-month.txt 50 totalflares-per-month-smoothed.svg
+../smooth_graph/smooth_graph.lua totalflares-per-month.txt 50 totalflares-per-month-smoothed.svg dontusekeys=true
+# dontusekeys because I need to not wrap them in quotes or something
+# xtics=time "timefmt=%Y-%m" "format={x='%Y'}"
